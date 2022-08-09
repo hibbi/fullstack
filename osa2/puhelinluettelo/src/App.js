0931778384ae050aa6eef32c0e2be150f75595b1
hibbi <samuli.halen@gmail.com>
2022-08-09 15:13:08 +0300

    2.6: puhelinluettelo step1

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const Button = ({ type, text }) => {
+  return <button type={type}>{text}</button>;
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [newName, setNewName] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+    console.log(event.target.value)
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+      const nameObject = {
+    name: newName,
+    id: persons.length + 1,
+  }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="inputName">name:</label>
+          <input
+            id="inputName"
+            value={newName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <Button type="submit" text="add" />
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <p key={person.id}>
+            {person.name}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
