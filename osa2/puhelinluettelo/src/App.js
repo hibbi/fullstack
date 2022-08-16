200aa06fc9507254a6b2c85c29e58538479e0edf
hibbi <samuli.halen@gmail.com>
2022-08-16 13:55:44 +0300

    2.10: puhelinluettelo step5

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,20 +4,83 @@ const Button = ({ type, text }) => {
   return <button type={type}>{text}</button>;
 };
 
+const Filter = (props) => {
+  //console.log(props.value);
+  return (
+    <>
+      <div>
+        <input
+          id="filterSearch"
+          value={props.value}
+          onChange={props.handle}
+          type="search"
+          
+        />
+      </div>
+    </>
+  );
+};
+
+const Persons = ({ persons, filter }) => {
+  //console.log(persons, filter);
+  const filteredPersons = persons.filter(person => {
+    return person.name.toLowerCase().includes(filter); 
+  })
+  return (
+    <div>
+      {filteredPersons.length > 0 ?
+      filteredPersons.map((person) => (
+        <p key={person.id}>
+          {person.name} {person.phone}
+        </p>
+      )) :
+        <h3>Haulla ei löytynyt nimiä.</h3>
+    }
+    </div>
+  );
+};
+
+const PersonForm = ( props ) => {
+  return (
+    <form onSubmit={props.onsubmit}>
+      <div>
+        <label htmlFor="inputName">name:</label>
+        <input id="inputName" value={props.newName} onChange={props.handle} />
+      </div>
+      <div>
+        <label htmlFor="inputPhone">number:</label>
+        <input
+          id="inputPhone"
+          type="tel"
+          value={props.newNumber}
+          onChange={props.handle}
+        />
+      </div>
+      <div>
+        <Button type="submit" text="add" />
+      </div>
+    </form>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", id: 1, phone: "040-1231244" },
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newQ, setNewQ] = useState("");
+
+  const HandleSearchChange = (event) => {
+    setNewQ(event.target.value);
+  };
 
   const handleInputChange = (event) => {
     if (event.target.id === "inputPhone") {
       setNewNumber(event.target.value);
-      console.log("PHONE", event.target.value);
     } else {
       setNewName(event.target.value);
-      console.log("NAME", event.target.value);
     }
   };
 
@@ -46,32 +109,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="inputName">name:</label>
-          <input id="inputName" value={newName} onChange={handleInputChange} />
-        </div>
-        <div>
-          <label htmlFor="inputPhone">number:</label>
-          <input
-            id="inputPhone"
-            type="tel"
-            value={newNumber}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <Button type="submit" text="add" />
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {persons.map((person) => (
-          <p key={person.id}>
-            {person.name} {person.phone}
-          </p>
-        ))}
-      </div>
+      <Filter value={newQ} handle={HandleSearchChange} />
+      <h3>add a new</h3>
+      <PersonForm
+        onsubmit={handleSubmit}
+        newName={newName}
+        newNumber={newNumber}
+        handle={handleInputChange}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={persons} filter={newQ} />
     </div>
   );
 };
